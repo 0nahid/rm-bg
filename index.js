68f28fb54ddbe672d13b1cd52006eded8005d592
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-09-01 14:26:02 +0600

    node

---
+++ index.js
@@ -0,0 +1,26 @@
+const os = require('os')
+const cp = require('child_process')
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const request = require('request')
+const removebg = require('removebg-id')
+
+const app = express()
+const port = process.env.PORT || 8000 || 3000
+
+
+// RemoveBG
+app.get('/api/removebg', (req, res) => {
+	let url = req.query.url
+	if (!(url || isUrl(url))) return res.status(400).json({ message: "Input parameter url" })
+	removebg.FromUrl(url, '2mZbr62TiNKYw3rFPPtb4BYn').then(async () => res.status(200).sendFile(process.cwd() + '/hasil-url.png')).catch(err => res.status(400).json({ error: String(err) }))
+})
+
+
+app.listen(port, () => {
+	console.log(App listening at http://localhost:${port})
+	
+})
